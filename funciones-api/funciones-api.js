73efe26b09ec1cd0982fbb9de46e0b5171c3c5af
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-06-01 18:56:33 +0200

    test ArtistPage con api

--- funciones-api/funciones-api.js
+++ funciones-api/funciones-api.js
@@ -1,23 +1,17 @@
-const URL = 'https://api.deezer.com/search/artist?q=Architects';
+const URL = 'https://corsproxy.io/?https://api.deezer.com/search/artist?q=Architects';
 
 const getArtist = async () => {
   try {
     const response = await fetch(URL, {
-      method: 'GET',
+      method: 'GET'
     });
 
     const data = await response.json();
     return data;
 
   } catch (error) {
-    console.log(`Error de búsqueda: ${error}`);
+    console.log(`⚠️ Error => ${error}`);
   }
 }
-
-(async () => {
-  const grupo = await getArtist();
-  console.log(grupo.data[0]);
-  // console.log(grupo);
-})();
 
 export { getArtist }
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import ArtistPage from './ArtistPageComponent/ArtistPage.jsx'
 
 function App() {
 
   return (
     <>
-      
+      <div className='d-flex justify-content-center align-items-center container-fluid min-vh-100'>
+        <ArtistPage /> 
+      </div>
     </>
   )
 }
